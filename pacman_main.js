b4ff027fe412ec340fa547058e52fe3e2ac6ab6d
CodySayer <codymathewsayer@gmail.com>
2019-04-24 11:53:54 -0700

    changed database implementation to MongoLab for use on Heroku

--- pacman_main.js
+++ pacman_main.js
@@ -5,31 +5,68 @@ const bodyParser = require('body-parser');
 const maper = require('./pacman_read_file')
 const cookieParser = require('cookie-parser');
 const bcrypt = require("bcrypt");
+const mongodb = require("mongodb");
 
 var app = express();
 
-const db_code = "mongodb+srv://client:passwerd1@cluster0-4kbn7.mongodb.net/test?retryWrites=true"
-const port = process.env.PORT || 8080;
+// Create a database variable outside of the database connection callback to reuse the connection pool
+var db;
 
+// Connect to the database before starting the application server
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/forum", function (err, client) {
+if (err) {
+    console.log(err);
+    process.exit(1);
+}
+
+// Save databse object from the callback for reuse
+db = client.db();
+console.log("Database connection ready");
+
+// Initialize the app
+var server = app.listen(process.env.PORT || 8080, function () {
+    var port = server.address().port;
+    console.log("App now running on port ", port);
+});
+});
+
+// Register partials
+hbs.registerPartials(__dirname + '/views/partials');
+
+// Parser
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+var distDir = __dirname + "/dist/";
+app.use(express.static(distDir));
+
+// collection names for later user
+const POSTS_COLLECTION = "posts";
+const USERS_COLLECTION = "Users";
+
+// Register views directory
 app.use(express.static('views')); 
 
+// Register styles directory
 app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static('public'))
+
+// Helpers
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+hbs.registerHelper('message', (text) => {
+    return text.toUpperCase();
+});
 
 app.post('/login', (request, response) => {
     var username = request.body.username;
     var password = request.body.password;
-    MongoClient.connect(db_code, {useNewUrlParser: true}, (err, client) => {
-        if (err) {
-            console.error('Error occured: ', err);
-        } else {
-            var database = client.db("PAC-MAN")
-            database.collection("Users").find({
+            db.collection(USERS_COLLECTION).find({
                 username: username
             }).toArray((err, result) => {
                 if (err || result[0] == undefined) {
@@ -52,8 +89,6 @@ app.post('/login', (request, response) => {
                     }
                 }
             });
-        }
-    })
 })
 
 app.get('/register', (request, response) => {
@@ -68,12 +103,7 @@ app.post('/register', (request, response) => {
     if (username.includes("<") || password.includes("<") || username.length > 12) {
         response.redirect('/register')
     } else {
-        MongoClient.connect(db_code, {useNewUrlParser: true}, (err, client) => {
-            if (err) {
-                console.error('Error occured: ', err);
-            } else {
-                var database = client.db("PAC-MAN")
-                database.collection("Users").findOne({
+                db.collection(USERS_COLLECTION).findOne({
                     username: username,
                 }).then (function (doc) {
                     if (doc !== null) {
@@ -81,7 +111,7 @@ app.post('/register', (request, response) => {
                             errortext: "User already exists."
                         })
                     } else {
-                        database.collection("Users").insertOne({
+                        db.collection(USERS_COLLECTION).insertOne({
                             username: username,
                             password: bcrypt.hashSync(password, 10),
                             highscore: 0
@@ -89,8 +119,6 @@ app.post('/register', (request, response) => {
                         response.redirect('/');
                     }
                 })
-            }
-        })
     }
 })
 
@@ -104,12 +132,7 @@ app.get('/', (request, response) => {
 app.post('/submit', (request, response) => {
     var username = request.body.username;
     var score = parseInt(request.body.score, 10);
-    MongoClient.connect(db_code, {useNewUrlParser: true}, (err, client) => {
-        if (err) {
-            console.error('Error occured: ', err);
-        } else {
-            var database = client.db("PAC-MAN")
-            database.collection("Users").findOne({
+            db.collection(USERS_COLLECTION).findOne({
                 username: username,
             }).then (function (doc) {
                 if (doc == null) {
@@ -119,7 +142,7 @@ app.post('/submit', (request, response) => {
                 } else if (doc.highscore > score) {
                     response.redirect('/pacman')
                 } else {
-                    database.collection("Users").updateOne({
+                    db.collection(USERS_COLLECTION).updateOne({
                         username: username
                     }, {
                         $set: {"highscore": score}
@@ -128,20 +151,72 @@ app.post('/submit', (request, response) => {
                     response.redirect('/pacman')
                 }
             })
-        }
-    })
 })
+
+app.post('/chat/submit', function (request, response) {
+    var monthNames = [
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+    ];
+    var date = new Date()
+    var day = date.getDate();
+    var monthIndex = date.getMonth();
+    var year = date.getFullYear();
+    var date = day + ' ' + monthNames[monthIndex] + ' ' + year;
+
+    var postid = Math.floor(Math.random() * 90000000) + 10000000;
+    var username = request.body.username;
+    var content = request.body.content;
+    db.collection(POSTS_COLLECTION).insertOne({
+        postid: postid,
+        datetime: date,
+        username: username,
+        content: content
+    }, (err, result) => {
+        if (err) {
+            response.send('Unable to create post');
+        }
+        var cursor = db.collection(POSTS_COLLECTION).find().toArray((err, result) => {
+            if (err) {
+                response.send('Unable to get posts');
+            }
+            var content = [];
+            var count = Object.keys(result).length;
+            for (i = 0; i < count; i++) {
+                content[i] = ['Posted by ' + JSON.stringify(result[i].username) +
+                    ' on ' + JSON.stringify(result[i].datetime) + '<br>' +
+                    '&nbsp;&nbsp; Message: ' + JSON.stringify(result[i].content)
+                ];
+            };
+            var content = content.join('<br> <br>');
+        });
+    });
+});
 
 app.get('/pacman', (request, response) => {
     if (request.cookies.username == undefined) {
         response.redirect('/')
     } else {
-        MongoClient.connect(db_code, {useNewUrlParser: true}, (err, client) => {
+        var cursor = db.collection(POSTS_COLLECTION).find().toArray((err, result) => {
             if (err) {
-                console.error('Error occured: ', err);
-            } else {
-                var database = client.db("PAC-MAN")
-                database.collection("Users").find({}).sort({highscore:-1}).limit(10).toArray(function (err, result) {
+                response.send('Unable to get posts');
+            }
+            var content = [];
+            var count = Object.keys(result).length;
+            for (i = 0; i < count; i++) {
+                content[i] = ['Post ' + JSON.stringify(result[i].postid) +
+                    ' - Posted by ' + JSON.stringify(result[i].username) +
+                    ' on ' + JSON.stringify(result[i].datetime) + '<br>' +
+                    '&nbsp;&nbsp; Message: ' + JSON.stringify(result[i].content)
+                ];
+            };
+            var content = content.join('<br> <br>');
+            return content
+        });
+
+                db.collection(USERS_COLLECTION).find({}).sort({highscore:-1}).limit(10).toArray(function (err, result) {
                     highscores = result.map(user => ({username: user.username, highscore: user.highscore}))
                     highscores = highscores.map(function(highscore) {
                         stringscore = highscore['highscore'].toString()
@@ -149,21 +224,16 @@ app.get('/pacman', (request, response) => {
                         return highscore['username'] + " ".repeat(spaces) + highscore['highscore']
                     })
                     highscores = highscores.join("\n")
-                    response.render('pacman.hbs',{
-                        values:maper.map(),
+                    response.render('pacman.hbs', {
+                        // content: content,
+                        values: maper.map(),
                         width: 28,
                         highscores: highscores,
                         username: request.cookies.username[0],
                         highscore: request.cookies.username[1]
                     });
                 })
-            }
-        })
     }
 })
-
-app.listen(port, () => {
-    console.log(`Server is up and listening on port ${port}`);
-});
 
 
