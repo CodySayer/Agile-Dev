a126ad5ae6bda8af607cfcae2da26b662391ef03
CodySayer <codymathewsayer@gmail.com>
2019-04-30 21:32:43 -0700

    working on chat functionality

--- pacman_main.js
+++ pacman_main.js
@@ -14,20 +14,20 @@ var db;
 
 // Connect to the database before starting the application server
 mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/forum", function (err, client) {
-if (err) {
-    console.log(err);
-    process.exit(1);
-}
+    if (err) {
+        console.log(err);
+        process.exit(1);
+    }
 
-// Save databse object from the callback for reuse
-db = client.db();
-console.log("Database connection ready");
+    // Save databse object from the callback for reuse
+    db = client.db();
+    console.log("Database connection ready");
 
-// Initialize the app
-var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port ", port);
-});
+    // Initialize the app
+    var server = app.listen(process.env.PORT || 8080, function () {
+        var port = server.address().port;
+        console.log("App now running on port ", port);
+    });
 });
 
 // Register partials
@@ -153,6 +153,29 @@ app.post('/submit', (request, response) => {
             })
 })
 
+//! Start chat code
+app.get('/chat', function (request, response) {
+    var cursor = db.collection('posts').find().toArray((err, result) => {
+        if (err) {
+            response.send('Unable to get posts');
+        }
+        var content = [];
+        var count = Object.keys(result).length;
+        for (i = 0; i < count; i++) {
+            content[i] = ['Posted by ' + JSON.stringify(result[i].username) +
+                ' on ' + JSON.stringify(result[i].datetime) + '<br>' +
+                '&nbsp;&nbsp; Message: ' + JSON.stringify(result[i].content)
+            ];
+        };
+        var content = content.join('<br> <br>');
+        response.render('pacman.hbs', {
+            year: new Date().getFullYear(),
+            content: content
+        });
+
+    });
+});
+
 app.post('/chat/submit', function (request, response) {
     var monthNames = [
         "January", "February", "March",
@@ -178,61 +201,31 @@ app.post('/chat/submit', function (request, response) {
         if (err) {
             response.send('Unable to create post');
         }
-        var cursor = db.collection(POSTS_COLLECTION).find().toArray((err, result) => {
-            if (err) {
-                response.send('Unable to get posts');
-            }
-            var content = [];
-            var count = Object.keys(result).length;
-            for (i = 0; i < count; i++) {
-                content[i] = ['Posted by ' + JSON.stringify(result[i].username) +
-                    ' on ' + JSON.stringify(result[i].datetime) + '<br>' +
-                    '&nbsp;&nbsp; Message: ' + JSON.stringify(result[i].content)
-                ];
-            };
-            var content = content.join('<br> <br>');
-        });
     });
 });
+//! End chat code
 
 app.get('/pacman', (request, response) => {
     if (request.cookies.username == undefined) {
         response.redirect('/')
     } else {
-        var cursor = db.collection(POSTS_COLLECTION).find().toArray((err, result) => {
-            if (err) {
-                response.send('Unable to get posts');
-            }
-            var content = [];
-            var count = Object.keys(result).length;
-            for (i = 0; i < count; i++) {
-                content[i] = ['Post ' + JSON.stringify(result[i].postid) +
-                    ' - Posted by ' + JSON.stringify(result[i].username) +
-                    ' on ' + JSON.stringify(result[i].datetime) + '<br>' +
-                    '&nbsp;&nbsp; Message: ' + JSON.stringify(result[i].content)
-                ];
-            };
-            var content = content.join('<br> <br>');
-            return content
+        db.collection(USERS_COLLECTION).find({}).sort({highscore:-1}).limit(10).toArray(function (err, result) {
+            highscores = result.map(user => ({username: user.username, highscore: user.highscore}))
+            highscores = highscores.map(function(highscore) {
+                stringscore = highscore['highscore'].toString()
+                spaces = 29 - (highscore['username'].length + stringscore.length)
+                return highscore['username'] + " ".repeat(spaces) + highscore['highscore']
+            })
+            highscores = highscores.join("\n");
+            response.render('pacman.hbs', {
+                content: content,
+                values: maper.map(),
+                width: 28,
+                highscores: highscores,
+                username: request.cookies.username[0],
+                highscore: request.cookies.username[1]
+            });
         });
-
-                db.collection(USERS_COLLECTION).find({}).sort({highscore:-1}).limit(10).toArray(function (err, result) {
-                    highscores = result.map(user => ({username: user.username, highscore: user.highscore}))
-                    highscores = highscores.map(function(highscore) {
-                        stringscore = highscore['highscore'].toString()
-                        spaces = 29 - (highscore['username'].length + stringscore.length)
-                        return highscore['username'] + " ".repeat(spaces) + highscore['highscore']
-                    })
-                    highscores = highscores.join("\n")
-                    response.render('pacman.hbs', {
-                        // content: content,
-                        values: maper.map(),
-                        width: 28,
-                        highscores: highscores,
-                        username: request.cookies.username[0],
-                        highscore: request.cookies.username[1]
-                    });
-                })
     }
 })
 
