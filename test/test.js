cb6aa8452045773e18ac4b8a0e5e55cac5320717
francisgau <40044801+francisgau@users.noreply.github.com>
2019-05-03 05:05:07 -0700

    testing

---
+++ test/test.js
@@ -0,0 +1,8 @@
+var pacman = require('pacman_read_file');
+describe('Map', function() {
+  describe('#maper.map()', function() {
+    it('pacman map length should be', function() {
+      assert.equal([1, 2, 3].indexOf(4), -1);
+    });
+  });
+});
