867c250cd44f7f2769bcd5fcf2186e3678d957fa
PsycoLove <44585075+PsycoLove@users.noreply.github.com>
2019-05-03 10:03:10 -0700

    unit

---
+++ test/test.js
@@ -0,0 +1,12 @@
+var assert = require('assert')
+var pacman = require('./pacman_read_file');
+describe('Map', function() {
+  describe('map()', function() {
+    it('pacman map height should be 30', function() {
+      assert.equal(pacman.map().length, 30);
+    });
+    it('pacman map width should be 28', function() {
+    assert.equal(pacman.map()[0].length, 28);
+      });
+  });
+});
