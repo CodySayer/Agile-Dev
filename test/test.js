4297ce666bd254e85f3a27beeade6d8900d3726e
CodySayer <codymathewsayer@gmail.com>
2019-05-13 14:17:48 -0700

    added services tag

--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
 var chai = require('chai')
-var pacman = require('../pacman_read_file')
+var pacman = require('../pacman_read_file.js')
 var login_test = require('../tdd.js')
 assert = chai.assert;
 describe('Map', function() {
   describe('map()', function() {
     it('pacman map height should be 30', function() {
+      console.log(pacman.map());
       assert.equal(pacman.map().length, 30);
     });
     it('pacman map width should be 28', function() {
