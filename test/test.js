5f7d1f960352e1d64658a59dbc2cdbcdc48b7ec0
CodySayer <codymathewsayer@gmail.com>
2019-05-14 09:38:56 -0700

    Revert "Revert "removed test TDD code from test script""
    This reverts commit 89b1586d30990cddf4652820411f3b6287fe643e.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,5 @@
 var chai = require('chai')
 var pacman = require('../pacman_read_file.js')
-var login_test = require('../tdd.js')
 assert = chai.assert;
 describe('Map', function() {
   describe('map()', function() {
@@ -12,28 +11,4 @@ describe('Map', function() {
       assert.equal(pacman.map()[0].length, 28);
     });
   });
-});
-describe('TDD', function(){
-  describe('login()', function(){
-    it('Login should return a string', function(){
-      assert.typeOf(login_test.login("a","b"), 'string')
-    })
-    it('Login failure should be Access denied', function() {
-      assert.equal(login_test.login("a","b"), 'Access denied')
-    });
-    it('Login success should be Access granted', function(){
-      assert.equal(login_test.login("a","a"), 'Access granted');
-    });
-  });
-  describe('isPrime()', function(){
-    it('isPrime should return a string', function(){
-      assert.typeOf(login_test.isPrime(1), 'string')
-    });
-    it('isPrime should return statement that the number is a prime', function(){
-      assert.equal(login_test.isPrime(2), "2 is a prime number");
-    });
-    it("isPrime should return statement that the number isn't a prime", function(){
-      assert.equal(login_test.isPrime(10), "10 isn't a prime number");
-    });
-  })
 });
