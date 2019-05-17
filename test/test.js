3d82fd98361b77b3fe8f0d3ed46fc844f192e0d3
CodySayer <codymathewsayer@gmail.com>
2019-05-17 14:11:11 -0700

    still working on Travis

--- test/test.js
+++ test/test.js
@@ -1,11 +1,11 @@
-var chai = require('chai');
+var chai = require('chai')
 // to fix the issue with Travis CI we would need to have the endpoint somewhere else
 // requiring the server means Travis waits for the server to stop before it can finish
 // so it just waits till it times out
-var expect = require('../pacman_main').server;
-var supertest = require('supertest');
-var should = require('should');
-var pacman = require('../pacman_read_file.js');
+var expect = require('../pacman_main')
+var supertest = require('supertest')
+var should = require('should')
+var pacman = require('../pacman_read_file.js')
 assert = chai.assert;
 
 var server = supertest.agent("http://localhost:8080");
